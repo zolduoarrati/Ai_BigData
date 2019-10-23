294b9bdd3ac152fe0d72d4a52416394900775228
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-10-24 09:16:26 +1300

    union users

---
+++ BigQuery/union.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM `modular-sorter-256519.stackoverflow.users_usa`
+UNION ALL
+SELECT *
+FROM `modular-sorter-256519.stackoverflow.users_china`
+UNION ALL
+SELECT *
+FROM `modular-sorter-256519.stackoverflow.users_russia`
