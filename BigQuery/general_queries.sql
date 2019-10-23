cab84250a8a7a2dab528115c45597542bdcf5533
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-10-24 10:46:26 +1300

    delete users

--- BigQuery/general_queries.sql
+++ BigQuery/general_queries.sql
@@ -2,3 +2,65 @@ SELECT count(id) as total_users
 FROM `modular-sorter-256519.stackoverflow.users_usa`
 order by total_users desc
 ------------------------------
+--Delete from
+DELETE FROM `modular-sorter-256519.stackoverflow.users_usa` WHERE
+location='Mexico' or
+location='Paris' or
+location='Lebanon' or
+location='lebanon' or
+location='Berlin' or
+location='Amsterdam' or
+location='Peru' or
+location='Ontario' or
+location='Vancouver' or
+location='Manchester' or
+location='Rome' or
+location='Holland' or
+location='Athens' or
+location='athens' or
+location='Milan' or
+location='cairo' or
+location='Cairo' or
+location='Cambridge' or
+location='Oxford' or
+location='Birmingham' or
+location='mexico' or
+location='berlin' or
+location='moscow' or
+location='Moscow' or
+location='peru' or
+location='Niagara Falls' or
+location='ottawa' or
+location='milan' or
+location='ontario' or
+location='belfast' or
+location='mexico' or
+location='paris' or
+location='lebanon' or
+location='lebanon' or
+location='berlin' or
+location='amsterdam' or
+location='peru' or
+location='ontario' or
+location='vancouver' or
+location='manchester' or
+location='rome' or
+location='holland' or
+location='athens' or
+location='athens' or
+location='milan' or
+location='cairo' or
+location='cairo' or
+location='cambridge' or
+location='oxford' or
+location='birmingham' or
+location='mexico' or
+location='berlin' or
+location='moscow' or
+location='moscow' or
+location='peru' or
+location='niagara falls' or
+location='ottawa' or
+location='milan' or
+location='ontario' or
+location='belfast'
